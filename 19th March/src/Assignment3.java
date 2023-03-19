2cdd05c65bd64974f8b8b9193a5458cfa9f1b33c
pbplop29 <pbplop@gmail.com>
2023-03-19 12:05:47 +0530

    done assignment test

---
+++ 19th March/src/Assignment3.java
@@ -0,0 +1,13 @@
+public class Assignment3 {
+    // Spring is a lightweight opensource framework for Application development in Java
+    // Rod Johnson developed spring framework
+    // Provides a powerful MVC architecture
+    // Used for developing enterprise-level Java applications
+
+    //Steps to develop spring application
+    //    1. Create java proj
+    //    2. Load the jars
+    //    3. Create bean class
+    //    4. Create xl file to get props
+    //    5. Create test class
+}
